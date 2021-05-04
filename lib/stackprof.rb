4f9eadc6b0dd727ae7e180089f2b51472bb088df
Aaron Patterson <tenderlove@ruby-lang.org>
2021-05-04 12:47:51 -0700

    Convert StackProf.run / StackProf.result to Ruby
    This commit is kind of a yak shave.  I would like to add thread tracking
    to stackprof.  I want to allow people to optionally dump the thread for
    which the stack was collected on by doing `StackProf.result(threads: true)`
    Updating the signature for `result` was kind of hard, so I am
    refactoring it to be in Ruby.
    
    This commit technically breaks the case of someone doing:
    
    ```ruby
    StackProf.start(out: "some_file.data")
    ...
    StackProf.result
    ```
    
    But I think it's strange to ask the start function to dump the file
    rather than asking the result function.  The result function actually
    writes to the file, so I think it makes more sense to pass the file (or
    file name) to the result function.

--- lib/stackprof.rb
+++ lib/stackprof.rb
@@ -2,6 +2,28 @@ require "stackprof/stackprof"
 
 module StackProf
   VERSION = '0.2.17'
+
+  class << self
+    private :_results
+
+    def run(mode: :wall, out: nil, interval: nil, raw: nil, metadata: nil, debug: nil, &block)
+      raise unless block_given?
+
+      start(mode: mode, interval: interval, raw: raw, metadata: metadata, debug: nil)
+
+      begin
+        yield
+      ensure
+        stop
+      end
+
+      results out
+    end
+
+    def results(io = nil)
+      _results io
+    end
+  end
 end
 
 StackProf.autoload :Report, "stackprof/report.rb"
